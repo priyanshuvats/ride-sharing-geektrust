be6730526326c6881a824be86811526e01f73644
Priyanshu Vats <priyanshu.vats.mec19@gmail.com>
2023-06-15 13:17:55 +0530

    Added distance util methods

--- app/src/main/java/ride/sharing/geektrust/matchStrategies/IMatchStrategy.java
+++ app/src/main/java/ride/sharing/geektrust/matchStrategies/IMatchStrategy.java
@@ -2,7 +2,8 @@ package ride.sharing.geektrust.matchStrategies;
 
 import java.util.List;
 import ride.sharing.geektrust.entities.Driver;
+import ride.sharing.geektrust.exceptions.BadRequestException;
 
 public interface IMatchStrategy {
-    public List<Driver> matchDrivers(String riderId);
+    public List<Driver> matchDrivers(String riderId) throws BadRequestException;
 }
--- app/src/main/java/ride/sharing/geektrust/utils/DistanceUtil.java
+++ app/src/main/java/ride/sharing/geektrust/utils/DistanceUtil.java
@@ -1,13 +1,21 @@
 package ride.sharing.geektrust.utils;
 
+import lombok.NonNull;
 import ride.sharing.geektrust.dto.Coordinates;
 
 public class DistanceUtil {
-    public static double calculateDistance(Coordinates c1, Coordinates c2){
-        return 0;
+    public static double calculateDistance(@NonNull Coordinates c1, @NonNull Coordinates c2){
+        return Math.sqrt(Math.pow(c2.getX()-c1.getX(), 2) + Math.pow(c2.getY()-c2.getY(), 2));
     }
 
-    public static boolean insideBound(Coordinates c, Coordinates leftBound, Coordinates rightBound){
-        return false;
+    public static boolean insideBound(@NonNull Coordinates c, 
+            @NonNull Coordinates leftBound, @NonNull Coordinates rightBound){
+        int cx = c.getX();
+        int cy = c.getY();
+        if(cx<leftBound.getX() || cy<leftBound.getY() || 
+            cx>rightBound.getX() || cy>rightBound.getY()){
+            return false;
+        }
+        return true;
     }
 }
